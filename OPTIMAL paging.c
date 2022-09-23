2ef33e07601a8b7352ea682ae8278c303ac24b09
Akramjaved-55 <113933075+Akramjaved-55@users.noreply.github.com>
2022-09-23 11:40:43 -0700

    Update OPTIMAL paging.c

--- OPTIMAL paging.c
+++ OPTIMAL paging.c
@@ -1,89 +1,89 @@
-#include <stdio.h>
-int search(int key, int frame_items[], int
-frame_occupied)
-{
-for (int i = 0; i < frame_occupied; i++)
-if (frame_items[i] == key)
-return 1;
-return 0;
-}
-void printOuterStructure(int max_frames){
-printf("Stream ");
-for(int i = 0; i < max_frames; i++)
-printf("Frame%d ", i+1);
-}
-void printCurrFrames(int item, int frame_items[],
-int frame_occupied, int max_frames)
-{
-printf("\n%d \t\t", item);
-for(int i = 0; i < max_frames; i++){
-if(i < frame_occupied)
-printf("%d \t\t", frame_items[i]);
-else
-printf("- \t\t");
-}
-}
-int predict(int ref_str[], int frame_items[], int
-refStrLen, int index, int frame_occupied)
-{
-int result = -1, farthest = index;
-for (int i = 0; i < frame_occupied; i++) {
-int j;
-for (j = index; j < refStrLen; j++)
-{
-if (frame_items[i] == ref_str[j])
-{
-if (j > farthest) {
-farthest = j;
-result = i;
-}
-break;
-}
-}
-if (j == refStrLen)
-return i;
-}
-return (result == -1) ? 0 : result;
-}
-void optimalPage(int ref_str[], int refStrLen, int
-frame_items[], int max_frames)
-{
-int frame_occupied = 0;
-printOuterStructure(max_frames);
-int hits = 0;
-for (int i = 0; i < refStrLen; i++) {
-if (search(ref_str[i], frame_items,
-frame_occupied)) {
-hits++;
-printCurrFrames(ref_str[i], frame_items,
-frame_occupied, max_frames);
-continue;
-}
-if (frame_occupied < max_frames){
-frame_items[frame_occupied] = ref_str[i];
-frame_occupied++;
-printCurrFrames(ref_str[i], frame_items,
-frame_occupied, max_frames);
-}
-else {
-int pos = predict(ref_str, frame_items,
-refStrLen, i + 1, frame_occupied);
-frame_items[pos] = ref_str[i];
-printCurrFrames(ref_str[i], frame_items,
-frame_occupied, max_frames);
-}
-}
-printf("\n\nHits: %d\n", hits);
-printf("Misses: %d", refStrLen - hits);
-}
+#include<stdio.h>
 int main()
 {
-int ref_str[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2,
-1, 2, 0, 1, 7, 0, 1};
-int refStrLen = sizeof(ref_str) / sizeof(ref_str[0]);
-int max_frames = 3;
-int frame_items[max_frames];
-optimalPage(ref_str, refStrLen, frame_items,
-max_frames);
-return 0;
+    int no_of_frames, no_of_pages, frames[10], pages[30], temp[10], flag1, flag2, flag3, i, j, k, pos, max, faults = 0;
+    printf("Enter number of frames: ");
+    scanf("%d", &no_of_frames);
+    
+    printf("Enter number of pages: ");
+    scanf("%d", &no_of_pages);
+    
+    printf("Enter page reference string: ");
+    
+    for(i = 0; i < no_of_pages; ++i){
+        scanf("%d", &pages[i]);
+    }
+    
+    for(i = 0; i < no_of_frames; ++i){
+        frames[i] = -1;
+    }
+    
+    for(i = 0; i < no_of_pages; ++i){
+        flag1 = flag2 = 0;
+        
+        for(j = 0; j < no_of_frames; ++j){
+            if(frames[j] == pages[i]){
+                   flag1 = flag2 = 1;
+                   break;
+               }
+        }
+        
+        if(flag1 == 0){
+            for(j = 0; j < no_of_frames; ++j){
+                if(frames[j] == -1){
+                    faults++;
+                    frames[j] = pages[i];
+                    flag2 = 1;
+                    break;
+                }
+            }    
+        }
+        
+        if(flag2 == 0){
+         flag3 =0;
+        
+            for(j = 0; j < no_of_frames; ++j){
+             temp[j] = -1;
+            
+             for(k = i + 1; k < no_of_pages; ++k){
+             if(frames[j] == pages[k]){
+             temp[j] = k;
+             break;
+             }
+             }
+            }
+            
+            for(j = 0; j < no_of_frames; ++j){
+             if(temp[j] == -1){
+             pos = j;
+             flag3 = 1;
+             break;
+             }
+            }
+            
+            if(flag3 ==0){
+             max = temp[0];
+             pos = 0;
+            
+             for(j = 1; j < no_of_frames; ++j){
+             if(temp[j] > max){
+             max = temp[j];
+             pos = j;
+             }
+             }            
+            }
+frames[pos] = pages[i];
+faults++;
+        }
+        
+        printf("\n");
+        
+        for(j = 0; j < no_of_frames; ++j){
+            printf("%d\t", frames[j]);
+        }
+    }
+    
+    printf("\n\nTotal Page Faults = %d", faults);
+    
+    return 0;
 }
